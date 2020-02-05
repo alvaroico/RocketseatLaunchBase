c048b6b66b7ddbdde4f02f0714e6ff215feb90f4
alvaroico <alvaro.r.p@hotmail.com>
2020-02-04 22:48:57 -0300

    ARRAY OU VETORES

--- objeto.js
+++ objeto.js
@@ -2,6 +2,8 @@
 // das notas entre os alunos e envia
 // mensagem de calculo da media.
 
+// OBJETO
+/*
 const aluno01 = {
     nome: "Mayk",
     nota: 9.8
@@ -18,5 +20,25 @@ const aluno03 = {
 }
 
 const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+console.log(media)
+*/
+// ARRAY OU VETORES
+const alunos = [
+    { 
+        nome: "Mayk",
+        nota: 9.8
+    },
+    { 
+        nome: "Diego",
+        nota: 10
+    },
+    {
+        nome: "Fulano",
+        nota: 2
+    }
+]
+
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 
 console.log(media)
