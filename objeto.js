761a0ff3ae9011aa4dbf19fd532ee693b0aa746a
alvaroico <alvaro.r.p@hotmail.com>
2020-02-04 22:41:00 -0300

    Objeto

---
+++ objeto.js
@@ -0,0 +1,22 @@
+// criar um programa que calcula a media
+// das notas entre os alunos e envia
+// mensagem de calculo da media.
+
+const aluno01 = {
+    nome: "Mayk",
+    nota: 9.8
+}
+
+const aluno02 = {
+    nome: "Diegok",
+    nota: 10
+}
+
+const aluno03 = {
+    nome: "Fulano",
+    nota: 2
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+console.log(media)
