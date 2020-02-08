4db0b9b7c3849bf15076de9809ad7243380425e7
alvaroico <alvaro.r.p@hotmail.com>
2020-02-08 12:04:41 -0300

    Boleanos

---
+++ boleanos.js
@@ -0,0 +1,91 @@
+// Marca aluno como reprovado se a nota for menor que 5
+// e tambem envia uma mensagem
+
+const alunosDaTurmaA = [
+    { 
+        nome: "Mayk",
+        nota: 9.8
+    },
+    { 
+        nome: "Diego",
+        nota: 10
+    },
+    {
+        nome: "Fulano",
+        nota: 2
+    }
+]
+const alunosDaTurmaB = [
+    { 
+        nome: "Cleiton",
+        nota: 10
+    },
+    { 
+        nome: "Robson",
+        nota: 10
+    },
+    {
+        nome: "Siclano",
+        nota: 0
+    },
+    {
+        nome: "Novo Aluno",
+        nota: 5
+    }
+]
+
+
+function calculoMedia(alunos){
+    let soma = 0
+    for ( let i = 0; i <alunos.length; i++){
+        soma = soma + alunos[i].nota
+    }
+
+    const media = soma / alunos.length
+
+    return media
+    
+    //return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+}
+
+const media1 = calculoMedia(alunosDaTurmaA)
+const media2 = calculoMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma){
+    // se a media for maior que 5, parabenizar a turma
+    if (media > 5 ){
+        console.log(`A media da ${turma} foi de ${media}. Parabéns`)
+    }else {
+        console.log(`A média da ${turma} é menor que 5`)
+    }
+}
+
+enviaMensagem(media1, 'turma A')
+enviaMensagem(media2, 'turma B')
+
+
+function marcarConmoReprovador(aluno) {
+    aluno.reprovado = false;
+    if(aluno.nota < 5 ) {
+        aluno.reprovado = true;
+    }
+
+    //console.table(alunos)
+}
+
+
+function enviaMensagemReprovado (aluno) {
+    if(aluno.reprovado){
+        console.log(`o Aluno ${aluno.nome} esta reprovado!`)
+    }
+}
+
+function alunoReprovado (alunos){
+    for(let aluno of alunos) {
+        marcarConmoReprovador(aluno);
+        enviaMensagemReprovado(aluno)
+    }
+}
+
+alunoReprovado(alunosDaTurmaA)
+alunoReprovado(alunosDaTurmaB)
