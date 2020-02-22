8a7ae7dfcc0bc32a110693db5b997278e2bafd7f
alvaroico <alvaro.r.p@hotmail.com>
2020-02-21 21:55:23 -0300

    Alinhado

--- iniciando-no-Back-end/modulo03/data.js
+++ iniciando-no-Back-end/modulo03/data.js
@@ -7,13 +7,13 @@ module.exports = [
     },
     {
         id: "GGyp_XPhEGo",
-        title: "2 Formulario Animado com JS puro e CSS Alvaroico",
+        title: "2 orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, CSS Alvaroico",
         duration: "30 minutos",
         price: "Free"
     },
     {
         id: "9F-mKTl1njg",
-        title: "3 Formulario Animado com JS puro e CSS Alvaroico",
+        title: "3 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 200 CSS Alvaroico",
         duration: "07 minutos",
         price: "Free"
     },
@@ -25,13 +25,13 @@ module.exports = [
     },
     {
         id: "Dh5TFPuJXKw",
-        title: "5 Formulario Animado com JS puro e CSS Alvaroico",
+        title: "5 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words whi CSS Alvaroico",
         duration: "57 minutos",
         price: "Free"
     },
     {
         id: "iUzxPkr1cWA",
-        title: "6 Formulario Animado com JS puro e CSS Alvaroico",
+        title: "6 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 fromo e CSS Alvaroico",
         duration: "57 minutos",
         price: "Free"
     }
