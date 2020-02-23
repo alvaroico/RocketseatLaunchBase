fafbcac28e46a658938c1b38439c0a3140dff1bf
alvaroico <alvaro.r.p@hotmail.com>
2020-02-23 19:52:59 -0300

    Separando as funções das rotas

---
+++ contrele-de-academia/modulo04/instructors.js
@@ -0,0 +1,13 @@
+exports.post = function(req, res){
+    //req.query
+    // req.body
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if(req.body[key] == ""){
+            return res.send('Por favor preencha todos os dados')
+        }
+    }
+
+    return res.send(req.body)
+}
