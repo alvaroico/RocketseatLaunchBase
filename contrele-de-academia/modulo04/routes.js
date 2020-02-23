7489f95047a4cda4d61826c29ee641d01e4fbf41
alvaroico <alvaro.r.p@hotmail.com>
2020-02-23 19:48:04 -0300

    Validando dados no Back end

--- contrele-de-academia/modulo04/routes.js
+++ contrele-de-academia/modulo04/routes.js
@@ -15,6 +15,14 @@ routes.get('/instructors/create', function(reg, res){
 routes.post("/instructors", function(req, res){
     //req.query
     // req.body
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if(req.body[key] == ""){
+            return res.send('Por favor preencha todos os dados')
+        }
+    }
+
     return res.send(req.body)
 })
 
