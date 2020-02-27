4154cdc85bd385db02b98c6ae8bc1601d17510ed
alvaroico <alvaro.r.p@hotmail.com>
2020-02-26 22:42:06 -0300

    Buscando instrutor com parâmetros da rota

--- contrele-de-academia/modulo04/instructors.js
+++ contrele-de-academia/modulo04/instructors.js
@@ -1,5 +1,23 @@
 const fs = require ("fs")
 const data = require("./data.json")
+
+exports.show = function(req, res){
+    
+    const { id } = req.params
+    
+
+    const foundInstructor = data.instructors.find(function(instructor){
+      return instructor.id == id
+        
+    })
+
+    if (!foundInstructor) return res.send("Instructor não encontrado")
+
+    return res.send(foundInstructor)
+
+}
+
+
 exports.post = function(req, res){
     //req.query
     // req.body
--- contrele-de-academia/modulo04/routes.js
+++ contrele-de-academia/modulo04/routes.js
@@ -13,7 +13,10 @@ routes.get('/instructors/create', function(reg, res){
     return res.render('instructors/create')
 })
 
-routes.post("/instructors", instructors.post)
+routes.get('/instructors/:id', instructors.show)
+
+
+routes.post('/instructors', instructors.post)
 
 routes.get('/members', function(req, res){
     return res.send("merembers")
