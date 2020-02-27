db4a9d36224017f2fd29fe7e3df182dd44c494d8
alvaroico <alvaro.r.p@hotmail.com>
2020-02-26 21:24:24 -0300

    Estruturando dados de instrutores

--- contrele-de-academia/modulo04/instructors.js
+++ contrele-de-academia/modulo04/instructors.js
@@ -1,3 +1,5 @@
+const fs = require ("fs")
+const data = require("./data.json")
 exports.post = function(req, res){
     //req.query
     // req.body
@@ -9,5 +11,30 @@ exports.post = function(req, res){
         }
     }
 
-    return res.send(req.body)
+    let {avatar_url, name, birth, gender, services} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    
+
+    data.instructors.push({
+        id,
+        name, 
+        avatar_url, 
+        birth, 
+        gender, 
+        services, 
+        created_at, 
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err){
+        if (err) return res.send("Erro ao gravar arquivo")
+
+        return res.redirect("/instructors")
+
+    })
+
+   // return res.send(req.body)
 }
