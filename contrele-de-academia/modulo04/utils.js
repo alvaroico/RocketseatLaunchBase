2f6d022f09c62d4cb65f0235156392177f6957c6
alvaroico <alvaro.r.p@hotmail.com>
2020-03-09 20:52:13 -0300

    Data OK

--- contrele-de-academia/modulo04/utils.js
+++ contrele-de-academia/modulo04/utils.js
@@ -20,9 +20,9 @@ module.exports = {
         // yyyy ano
         const year = date.getUTCFullYear()
         // mm mes
-        const month = date.getUTCMonth() + 1
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         // dd dia
-        const day = date.getUTCDate()
+        const day = `0${date.getUTCDate()}`.slice(-2)
         
         return `${year}-${month}-${day}`
         
