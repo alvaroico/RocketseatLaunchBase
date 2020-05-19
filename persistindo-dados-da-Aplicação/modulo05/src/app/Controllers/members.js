822857346aea3fdc446bbde15a76b8d04223c527
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-05-19 20:12:02 -0300

    Model de membros

--- persistindo-dados-da-Aplicação/modulo05/src/app/Controllers/members.js
+++ persistindo-dados-da-Aplicação/modulo05/src/app/Controllers/members.js
@@ -1,72 +1,68 @@
 const { age, date } = require("../lib/utils")
 
+
+const Member = require('../models/Member')
+
 module.exports = {
-index(req, res){
-            //console.log(data)
-            return res.render("members/index")
-},
-show(req, res){
-    return
-},
-create(req, res){
-    return res.render('members/create')
-
-},
-post(req, res){
- //req.query
-    // req.body
-    const keys = Object.keys(req.body)
-
-    for (key of keys){
-        if(req.body[key] == ""){
-            return res.send('Por favor preencha todos os dados')
-        }
-    }
-
-},
-edit(req, res){
-    return
-},
-put(req, res){
-   //req.query
-    // req.body
-    const keys = Object.keys(req.body)
-
-    for (key of keys){
-        if(req.body[key] == ""){
-            return res.send('Por favor preencha todos os dados')
-        }
-    }
-
-    let {avatar_url, name, birth, gender, services} = req.body
-
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
-
+    index(req, res){
+        Member.all(function(members){
+            return res.render("members/index",{ members})
+        })
+    },
+    show(req, res){
+        Member.find(req.params.id, function(member){
+            if (!member) return res.send("Member não encontrado!")
     
-
-    data.members.push({
-        id,
-        name, 
-        avatar_url, 
-        birth, 
-        gender, 
-        services, 
-        created_at, 
-    })
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err){
-        if (err) return res.send("Erro ao gravar arquivo")
-
-        return res.redirect("/members")
-
-    })
-
-   // return res.send(req.body)
-},
-delete(req, res){
-    return
-},
-
-}
+            member.birth = date(member.birth).birthDay
+            
+            return res.render("members/show", { member})
+        })
+    },
+    create(req, res){
+        return res.render('members/create')
+    
+    },
+    post(req, res){
+        const keys = Object.keys(req.body)
+    
+        for (key of keys){
+            if(req.body[key] == ""){
+                return res.send('Por favor preencha todos os dados')
+            }
+        }
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`)
+        })
+        
+    
+    },
+    edit(req, res){
+        Member.find(req.params.id, function(member){
+            if (!member) return res.send("Member não encontrado!")
+    
+            member.birth = date(member.birth).iso
+          
+            return res.render("members/edit", { member})
+        })
+    },
+    put(req, res){
+        const keys = Object.keys(req.body)
+    
+        for (key of keys){
+            if(req.body[key] == ""){
+                return res.send('Por favor preencha todos os dados')
+            }
+        }
+    
+        Member.update(req.body, function(){
+            return res.redirect(`/members/${req.body.id}`)
+        })
+        
+    },
+    delete(req, res){
+        Member.delete(req.body.id, function(){
+            return res.redirect(`/members`)
+        })
+    },
+    
+    }
