be22fc9392a896fcf5e71b3b727d1a30ab1fd422
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-05-25 13:25:22 -0300

    Algoritmo de paginação no front II

--- persistindo-dados-da-Aplicação/modulo05/public/scripts.js
+++ persistindo-dados-da-Aplicação/modulo05/public/scripts.js
@@ -6,3 +6,24 @@ for (item of menuItens){
         item.classList.add("active")
     }
 }
+
+// Paginação
+// totalPage = 20 
+// selectedpage = 15
+// [1, ..., 13, 14, 15, 16, 17, ..., 20 ]
+
+let totalPage = 20,
+selectedPage = 15.
+pages = []
+ for (let currentPage = 1; currentPage <= totalPage; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPage
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+     if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+         pages.push(currentPage)
+     }
+ }
+
+ console.log(pages)
