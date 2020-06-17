496fdc7a11b3d8737161e75c9155284184455740
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-16 23:17:47 -0300

    Conhecendo async-await e salvando no Banco

--- Launchstore/modulo06/src/app/Controllers/ProductController.js
+++ Launchstore/modulo06/src/app/Controllers/ProductController.js
@@ -1,4 +1,6 @@
 const Category = require('../models/Category')
+const Product = require('../models/Product')
+
 
 module.exports = {
     create(req, res){
@@ -19,8 +21,23 @@ module.exports = {
 
 
     },
-    post(req, res){
+    async post(req, res){
         // Logica de salvar
+      
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if(req.body[key] == ""){
+            return res.send('Por favor preencha todos os dados')
+        }
+    }
+    let results = await Product.create(req.body)
+    const productId = results.rows[0].id
+
+    results = await Category.all()
+    const categories = results.rows
+
+    return res.render("products/create.njk", { productId, categories })
 
     }
 }
