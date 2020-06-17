ee24e75a414517e8c93b9ef4ccaf65befb8cf5c7
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-16 21:16:11 -0300

    Finalizando estilo do formulário

--- Launchstore/modulo06/src/app/Controllers/ProductController.js
+++ Launchstore/modulo06/src/app/Controllers/ProductController.js
@@ -3,15 +3,8 @@ const Category = require('../models/Category')
 module.exports = {
     create(req, res){
         // Pegar categoria
-        Category.all()
-        .then(function(results){
+        return res.render("products/create.njk")
 
-            const categories = results.rows;
-            return res.render("products/create.njk", { categories })
-
-        }).catch(function(err){
-            throw new Error(err)
-        })
 
     },
     post(req, res){
