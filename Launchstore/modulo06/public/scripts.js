b94cdd011508bed75d48cd2d397b53e10cb982fa
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-17 18:22:00 -0300

    Formatando moeda

--- Launchstore/modulo06/public/scripts.js
+++ Launchstore/modulo06/public/scripts.js
@@ -4,7 +4,12 @@ input.addEventListener("keydown", function(e){
     let { value } = e.target
 
     value = value.replace(/\D/g,"")
+
+    value = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(value/100)
   
     e.target.value = value
   }, 1)
-})
+}) 
