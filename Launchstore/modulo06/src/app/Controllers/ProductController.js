70dfe057c5669937fc2742139571926014e25795
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-16 22:36:58 -0300

    Conhecendo promises

--- Launchstore/modulo06/src/app/Controllers/ProductController.js
+++ Launchstore/modulo06/src/app/Controllers/ProductController.js
@@ -2,8 +2,20 @@ const Category = require('../models/Category')
 
 module.exports = {
     create(req, res){
+      //Conhecendo promises
         // Pegar categoria
-        return res.render("products/create.njk")
+        Category.all()
+        .then(function(results){
+
+          const categories = results.rows
+          return res.render("products/create.njk", {categories})
+
+
+        }).catch(function(err){
+          console.log('Banco de dados Offline')
+            throw new Error(err)
+            
+        })
 
 
     },
--- Launchstore/modulo06/src/config/db.js
+++ Launchstore/modulo06/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg")
 
 module.exports = new Pool({
     user:'postgres',
-    password: "1234567890",
+    password: "123456",
     host: "localhost",
     port: 5432,
     database: "launchstoredb"
