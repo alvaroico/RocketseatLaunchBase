756941e9d77e55d5d63f186b7690944d32d5539f
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-18 23:05:46 -0300

    Atualizando lista de arquivos

--- UploadDeImagens/modulo7/public/scripts.js
+++ UploadDeImagens/modulo7/public/scripts.js
@@ -35,63 +35,81 @@ const Mask = {
 };
 
 const PhotosUploads = {
-  preview: document.querySelector('#photos-preview'),
+  input: "",
+  preview: document.querySelector("#photos-preview"),
   uploadLimit: 6,
+  files: [],
 
   handleFileInput(event) {
     const { files: FileList } = event.target;
-    if (PhotosUploads.hasLimit(event)) return
+    if (PhotosUploads.hasLimit(event)) return;
+
+    PhotosUploads.input = event.target
 
     Array.from(FileList).forEach((file) => {
+      
+      PhotosUploads.files.push(file)
+      
       const reader = new FileReader();
 
       reader.onload = () => {
         const image = new Image();
         image.src = String(reader.result);
 
-        const div = PhotosUploads.getContainer(image)
+        const div = PhotosUploads.getContainer(image);
 
-        PhotosUploads.preview.appendChild(div)
+        PhotosUploads.preview.appendChild(div);
       };
 
-      reader.readAsDataURL(file)
+      reader.readAsDataURL(file);
     });
+
+    PhotosUploads.input.file = PhotosUploads.getAllFiles()
   },
-  hasLimit(event){
-    const { uploadLimit } = PhotosUploads;
-    const { files: fileList  } = event.target
+  hasLimit(event) {
+    const { uploadLimit, input: fileList } = PhotosUploads;
 
     if (fileList.length > uploadLimit) {
       alert(`Envie no máximo ${uploadLimit} fotos`);
       event.preventDefault();
       return true;
     }
-    return false
+    return false;
+  },
+  getAllFiles(){
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+    PhotosUploads.files.forEach(file => dataTransfer.items.add(file))
+
+    return dataTransfer.files
+
   },
   getContainer(image) {
     const div = document.createElement("div");
-        div.classList.add('photo');
+    div.classList.add("photo");
 
-        div.onclick = PhotosUploads.removerPhoto
+    div.onclick = PhotosUploads.removerPhoto;
 
-        div.appendChild(image)
+    div.appendChild(image);
 
-        div.appendChild(PhotosUploads.getRemoverButton())
+    div.appendChild(PhotosUploads.getRemoverButton());
 
-        return div
+    return div;
   },
-  getRemoverButton(){
-    const button = document.createElement('i')
-    button.classList.add('material-icons')
-    button.innerHTML = "close"
-    return button
+  getRemoverButton() {
+    const button = document.createElement("i");
+    button.classList.add("material-icons");
+    button.innerHTML = "close";
+    return button;
   },
-  removerPhoto(event){
-    const photoDiv = event.target.parentNode
-    const photosArray = Array.from(PhotosUploads.preview.children)
-    const index = photosArray.indexOf(photoDiv)
+  removerPhoto(event) {
+    const photoDiv = event.target.parentNode; // <div class="photo"
+    const photosArray = Array.from(PhotosUploads.preview.children);
+    const index = photosArray.indexOf(photoDiv);
 
-    photoDiv.remove()
-  }
-  
+    PhotosUploads.files.splice(index, 1)
+    PhotosUploads.input.file = PhotosUploads.getAllFiles()
+
+    photoDiv.remove();
+  },
 };
