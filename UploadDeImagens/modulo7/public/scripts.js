6bae1f197cc891bae9fc5cd8a8f0be12b23b5011
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-21 01:08:48 -0300

    Funcionalidades da galeria de imagens

--- UploadDeImagens/modulo7/public/scripts.js
+++ UploadDeImagens/modulo7/public/scripts.js
@@ -207,3 +207,19 @@ return button
 },
 
 }
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(e) {
+        const { target } = e
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+
+        target.classList.add('active')
+
+        ImageGallery.highlight.src = target.src
+    }
+
+
+}
