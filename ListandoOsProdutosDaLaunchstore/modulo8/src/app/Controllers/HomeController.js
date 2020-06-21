ef68eb73b27f9f23700a1caf3db5ef2365bbba24
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-21 13:56:00 -0300

    Estruturando dados para o front end

--- ListandoOsProdutosDaLaunchstore/modulo8/src/app/Controllers/HomeController.js
+++ ListandoOsProdutosDaLaunchstore/modulo8/src/app/Controllers/HomeController.js
@@ -10,6 +10,25 @@ module.exports = {
 
     if (!products) return res.send("Produto nao encontrado!")
 
+    async function gerImage(productId) {
+      let results = await Product.files(product.id)
+      let files = results.rows.map(file =>`${req.protocol}://${req.headers.host}/${file.path.replace("public", "")}`)
+
+
+      return files[0]
+    }
+
+    const productsPromise = products.map(product => {
+      product.img = await getImage(product)
+      product.oldPrice = formatPrice(product.old_price)
+      product.price = formatPrice(product.price)
+      return product
+    })
+
+    const lastAdded = await Promise.all(productsPromise)
+
+    return res.render("home/index", { product: lastAdded })
+
   }
 
 }
