612fe0b35e33b210c341041943de77c8ed3c3320
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-03 21:22:29 -0300

    Rotas e Controllers de usuários

---
+++ CadastrandoUsuarios/modulo7.2/src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/Controllers/HomeController')
+
+const products = require('./products')
+const users = require('./users')
+
+//home
+routes.get('/', HomeController.index)
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+
+// Alias
+routes.get('/ads/create', function(req, res){
+    return res.redirect("/products/create")
+})
+
+module.exports = routes
