cc4f35e6b9b4f513e664b7c9f54ff97538343520
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-06 23:59:54 -0300

    Criptografia de senha

--- CadastrandoUsuarios/modulo7.2/src/app/Controllers/UserController.js
+++ CadastrandoUsuarios/modulo7.2/src/app/Controllers/UserController.js
@@ -11,14 +11,21 @@ class UserController {
 module.exports = new UserController()
 */
 
+const User = require("../models/User")
+
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register")
   },
+  show(req, res){
+    return res.send('Ok, Cadastrado!')
+  },
   async post(req, res) {
+
+    const userId = await User.create(req.body)
    
       
-    return res.send('Passou!')
+    return res.redirect('/users')
 
 
   }
