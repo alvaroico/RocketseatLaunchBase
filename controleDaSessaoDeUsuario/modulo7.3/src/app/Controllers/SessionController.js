b4a1425ad2f4ad859c727fdc594b4a2ef0870c1b
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-13 22:44:31 -0300

    Lógica de login

--- controleDaSessaoDeUsuario/modulo7.3/src/app/Controllers/SessionController.js
+++ controleDaSessaoDeUsuario/modulo7.3/src/app/Controllers/SessionController.js
@@ -2,6 +2,15 @@ module.exports = {
   loginForm(req,res){
     return res.render("session/index")
   },
+  login(req,res){
+    // verificar se o usuário ta cadastrado
+    // Verificar se o password bate
+    // colocar o usuário no req.session
+    req.session.userId = req.user.id
+
+    return res.redirect("/users")
+
+  },
   logout(req,res) {
     req.session.destroy()
     return res.redirect("/")
--- controleDaSessaoDeUsuario/modulo7.3/src/routes/users.js
+++ controleDaSessaoDeUsuario/modulo7.3/src/routes/users.js
@@ -3,12 +3,13 @@ const routes = express.Router()
 const SessionController = require('../app/Controllers/SessionController')
 const UserController = require('../app/Controllers/UserController')
 
-const Validator = require('../app/validators/user')
+const UserValidator = require('../app/Validators/user')
+const SessionValidator = require('../app/Validators/session')
 
 
 // Login/logout
 routes.get('/login',SessionController.loginForm)
-//routes.post('/login', SessionController.login)
+routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // reset password / forgot
@@ -21,10 +22,10 @@ routes.post('/logout', SessionController.logout)
 // user register UserController
 
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', Validator.show, UserController.show)
-routes.put('/',Validator.update, UserController.update)
+routes.get('/', UserValidator.show, UserController.show)
+routes.put('/',UserValidator.update, UserController.update)
 //routes.delete('/', UserController.delete)
 
 
