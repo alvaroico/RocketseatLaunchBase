c22f76e597014d584a98f0ce7c7c14b8cc006c33
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-15 22:16:47 -0300

    SQL cascade

--- controleDaSessaoDeUsuario/modulo7.3/src/app/Controllers/UserController.js
+++ controleDaSessaoDeUsuario/modulo7.3/src/app/Controllers/UserController.js
@@ -65,6 +65,21 @@ module.exports = {
         error: "Algum erro aconteceu!"
       })
     }
+  },
+  async delete(req, res){
+    try{
+      await User.delete(1) //req.body.id
+      req.session.destroy()
+      return res.render("session/login", {
+        success: 'Conta deletada com sucesso!'
+      })
+
+    }catch(err){
+      console.error(err)
+      return res.render("user/index", {
+        error: 'Error ao tentar deletar sua conta!'
+      })
+    }
   }
 }
 
