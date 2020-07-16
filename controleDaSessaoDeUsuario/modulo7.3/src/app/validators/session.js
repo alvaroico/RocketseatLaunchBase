949ce0e706fc4e7b0a2873474f5568b2934b0132
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-15 21:25:13 -0300

    Testando a recuperação de senha

--- controleDaSessaoDeUsuario/modulo7.3/src/app/validators/session.js
+++ controleDaSessaoDeUsuario/modulo7.3/src/app/validators/session.js
@@ -47,7 +47,7 @@ async function forgot(req, res, next){
 
 async function reset(req, res, next){
   // procura o usuário
-  const { email, password, token } = req.body
+  const { email, password, token, passwordRepeat } = req.body
 
     const user = await User.findOne({where: {email}})
 
