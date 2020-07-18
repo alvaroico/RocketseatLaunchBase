dd2694d417bf1f9bf811dce64c345725f6d07c95
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-17 21:15:02 -0300

    padrao-mvc

---
+++ NodeJSAvancado/modulo8/src/routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const routes = express.Router()
+const SessionController = require('../app/Controllers/SessionController')
+const UserController = require('../app/Controllers/UserController')
+
+const UserValidator = require('../app/Validators/user')
+const SessionValidator = require('../app/Validators/session')
+const {isLoggedRedirectToUsers, onlyUsers} = require('../app/middlewares/session')
+
+
+// Login/logout
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
+
+// reset password / forgot
+
+routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/password-reset', SessionController.resetForm)
+routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
+routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
+
+// user register UserController
+
+routes.get('/register', UserController.registerForm)
+routes.post('/register', UserValidator.post, UserController.post)
+
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
+routes.put('/',UserValidator.update, UserController.update)
+routes.delete('/', UserController.delete)
+
+
+
+module.exports = routes
+
+
