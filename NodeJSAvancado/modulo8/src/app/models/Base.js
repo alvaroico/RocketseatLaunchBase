63100a25d2cf12755dd6a2dcc46f445f464afec5
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-18 15:50:12 -0300

    Criando Base Model

--- NodeJSAvancado/modulo8/src/app/models/Base.js
+++ NodeJSAvancado/modulo8/src/app/models/Base.js
@@ -1,3 +1,5 @@
+const db = require('../../config/db')
+
 const Base = {
   init({ table }) {
     if(!table) throw new Error ('Parâmetro Invalido')
@@ -27,6 +29,53 @@ const Base = {
     const results = await db.query(query)
     return results.rows[0]
   },
+  async create(field){ //User.create({ name: 'Alvaro'})
+  try{
+      let keys = [],
+      values = []
+
+    Object.keys(field).map(key => {
+      keys.push(key)
+      values.push(fields[key])
+    })
+    
+    const query = `INSERT INTRO ${this.table} (${keys.join(',')})
+      VALUES (${values.join(',')})
+      RETURNING id
+    `
+
+    const results = await db.query(query)
+    return results.rows[0].id
+
+  } catch(error){
+    console.error(error)
+    }
+  },
+  update(id, fields){
+    try {
+      let update = []
+
+      Object.keys(fields).map(key => {
+
+        // category_id=($1)
+        const line = `${key} = '${fields[key]}'`
+        update.push(line)
+      })
+  
+      let query = `UPDATE ${this.table} SET
+      ${update.join(',')} where id = ${id}
+      `
+      return db.query(query)
+      
+      
+    } catch (error) {
+      console.error(error)
+    }
+    
+  },
+  delete(id){
+    return db.query('DELETE FROM products WHERE id = $1', [id])
+  }
 }
 
 module.exports = Base
